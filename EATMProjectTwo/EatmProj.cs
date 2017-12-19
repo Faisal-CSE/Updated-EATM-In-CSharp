e39b1e46e31f209b1d8ce7401d2dd194c2740b84
Faisal-CSE <porag_faisal123@hotmail.com>
2017-12-19 12:13:42 +0600

    EATM Console Application
    Full version

---
+++ EATMProjectTwo/EatmProj.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EATMProjectTwo
+{
+    class EatmProj
+    {
+        readonly int[] cardNumbers = new int[] { 1, 2, 3 };
+        readonly int[] pinNumbers = new int[] { 123, 234, 456 };
+        private int[] balances = new int[] { 500, 200, 800 };
+        private int cardInput;
+        private int pinNumber;
+        private int _withdrow;
+        private int _trantransactionCount = 1;
+
+        private bool flag = true;
+
+
+        public void EatmMachine()
+        {
+            Console.WriteLine(" *** Welcome to our EatmProj service *** ");
+            Console.Write("what is your card number : ");
+            cardInput = Convert.ToInt32(Console.ReadLine());
+
+            CheckCardNumber(cardInput);
+
+
+        }
+
+        public void CheckCardNumber(int cardNo)
+        {
+            Console.Write("Enter your valid pin number : ");
+            pinNumber = Convert.ToInt32(Console.ReadLine());
+            if (cardNumbers[0] == cardNo)
+            {
+                CheckPinNumber(pinNumber,pinNumbers,0);
+            }
+            else if (cardNumbers[1] == cardNo)
+            {
+                CheckPinNumber(pinNumber,pinNumbers,1);
+            }
+
+            else if (cardNumbers[2] == cardNo)
+            {
+                CheckPinNumber(pinNumber,pinNumbers,2);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid card number.Please try again !");
+            }
+        }
+
+
+        public void CheckPinNumber(int pinNum,int[] id,int index)
+        {
+            if (pinNum == id[index])
+            {
+                Console.WriteLine("1.Check account balance \t 2.Cash withdrawal \t 3.Exit");
+
+                MenuOptions(balances,index);
+            }
+            else if (pinNum == id[index])
+            {
+                Console.WriteLine("1.Check account balance \t 2.Cash withdrawal \t 3.Exit");
+
+                MenuOptions(balances, index);
+            }
+            else if (pinNum == id[index])
+            {
+                Console.WriteLine("1.Check account balance \t 2.Cash withdrawal \t 3.Exit");
+
+                MenuOptions(balances, index);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Pin number ");
+            }
+        }
+
+        public void MenuOptions(int[] id,int index)
+        {
+            do
+            {
+                Console.Write("What you want to do ? Enter option numbre : ");
+                int getOption = Convert.ToInt32(Console.ReadLine());
+                if (getOption == 1)
+                {
+                    Console.WriteLine("Your total balance is : " + id[index]);
+                }
+                else if (getOption == 2)
+                {
+                    Console.Write("Enter Amount : ");
+                    _withdrow = Convert.ToInt32(Console.ReadLine());
+                    if (_withdrow < id[index])
+                    {
+                        if (_trantransactionCount > 3)
+                        {
+                            Console.WriteLine("Total transactions limit is 3 per day.");
+                            flag = false;
+                        }
+                        else
+                        {
+                            Transaction(id, index);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Insufficient balance.Thank You !");
+                    }
+
+                }
+                else if (getOption == 3)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("You enter invalid option number ! Enter valid option");
+                }
+            } while (flag);
+
+        }
+
+        public void Transaction(int[] id,int index)
+        {
+            id[index] -= _withdrow;
+            _trantransactionCount++;
+            Console.WriteLine("Cash withdrawal successful. Your account balance is " + id[index]);
+            Console.WriteLine("Do you want to quit? Press 3 for quit ... ");
+            Console.WriteLine();
+        }
+    }
+}
+
